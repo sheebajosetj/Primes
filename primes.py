ae7992bb69da6e54cc7b822110a6aa75ae91ae58
sheebajosetj <sheebatjose@gmail.com>
2023-06-09 11:20:31 +0530

    efficient code

--- primes.py
+++ primes.py
@@ -1,20 +1,17 @@
 def is_prime(num):
-    if num < 2:
+    if num < 2:  # Numbers less than 2 are not prime
         return False
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
     return True
 
-def generate_primes(N):
-    primes = []
-    for num in range(2, N + 1):
-        if is_prime(num):
-            primes.append(num)
-    return primes
-
 # Example usage
-N = int(input("Enter a number: "))
-prime_numbers = generate_primes(N)
-print("Prime numbers up to", N, "are:", prime_numbers)
+number = int(input("Enter a number: "))
+if is_prime(number):
+    print(number, "is a prime number")
+else:
+    print(number, "is not a prime number")
+
+
 
